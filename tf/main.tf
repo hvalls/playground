f377d9a0d32ab2643ce6ea82a04d77df3db2ed4f
Héctor Valls Mondragón <hvalls198@gmail.com>
2024-06-18 12:42:47 +0200

    test

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,15 @@
 output "test" {
   value = "this is a test"
 }
+
+resource "null_resource" "example" {
+  # No specific configuration needed
+  triggers = {
+    # Trigger the provisioners whenever `message` changes
+    message = var.message
+  }
+
+  provisioner "local-exec" {
+    command = "echo Hello, World!"
+  }
+}
