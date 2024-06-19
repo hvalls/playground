299cf0fff2a4fcbbea97753f265a9146eb301ddc
Héctor Valls <hvalls@protonmail.com>
2024-06-19 17:09:09 +0200

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -6,7 +6,7 @@ resource "null_resource" "example" {
   # No specific configuration needed
   triggers = {
     # Trigger the provisioners whenever `message` changes
-    message = "this is the message"
+    message = "this is the message 2"
   }
 
   provisioner "local-exec" {
