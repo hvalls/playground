2fa1bd0a425b929da612e22190969845707a2535
Héctor Valls <hvalls@protonmail.com>
2024-06-19 17:12:46 +0200

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,8 +1,25 @@
 output "test" {
-  value = "this is a test"
+  value = "this is a test 1"
+}
+
+output "test2" {
+  value = "this is a test 2"
 }
 
 resource "null_resource" "example" {
+  # No specific configuration needed
+  triggers = {
+    # Trigger the provisioners whenever `message` changes
+    message = "this is the message 2"
+  }
+
+  provisioner "local-exec" {
+    command = "echo Hello, World!"
+  }
+}
+
+
+resource "null_resource" "example2" {
   # No specific configuration needed
   triggers = {
     # Trigger the provisioners whenever `message` changes
